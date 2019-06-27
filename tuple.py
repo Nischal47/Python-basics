b683f351cb44f691b67472006e9b3325efc7d009
Your Name <you@example.com>
2019-06-27 15:16:53 +0545

    Added tuple.py Learning about tuple on python

---
+++ tuple.py
@@ -0,0 +1,14 @@
+atuple=("a","b","c")#tuple is ordered but unchangable collection of data. ( ) is use to denote tuple instead of [ ] on list
+
+#print as
+for x in atuple:
+    print(x)
+#and
+print(atuple)
+#check by
+if "a" in atuple:
+    print("Yes")
+#count elements like
+print(atuple.count("a"))
+
+#items cant be deleted and append on tuple
